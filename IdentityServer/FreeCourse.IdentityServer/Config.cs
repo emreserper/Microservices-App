deea33aa75b788c35478cbc6b05bf36988e7c627
emreserper <s.emreserper@gmail.com>
2022-01-16 17:36:58 +0300

    Add - ApiResources

--- IdentityServer/FreeCourse.IdentityServer/Config.cs
+++ IdentityServer/FreeCourse.IdentityServer/Config.cs
@@ -10,6 +10,12 @@ namespace FreeCourse.IdentityServer
 {
     public static class Config
     {
+        public static IEnumerable<ApiResource> ApiResources => new ApiResource[]
+        {
+            new ApiResource("resource_catalog"){Scopes={"catalog_fullpermission"}},
+            new ApiResource("resource_photo_stock"){Scopes={"photo_stock_fullpermission"}},
+            new ApiResource(IdentityServerConstants.LocalApi.ScopeName)
+        };
         public static IEnumerable<IdentityResource> IdentityResources =>
                    new IdentityResource[]
                    {
@@ -19,7 +25,7 @@ namespace FreeCourse.IdentityServer
             new ApiScope[]
             {
                 new ApiScope("catalog_fullpermission","Full permission for Catalog API"),
-                new ApiScope("photo_fullpermission","Full permission for Photo Stock API"),
+                new ApiScope("photo_stock_fullpermission","Full permission for Photo Stock API"),
                 new ApiScope(IdentityServerConstants.LocalApi.ScopeName)
             };
 
